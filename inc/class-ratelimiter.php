f99ea090b82cc04cef5091fd0e945620a27f5632
Myrotvorets <support@myrotvorets.center>
2022-07-04 21:38:39 +0300

    Differentiate between search and view

--- inc/class-ratelimiter.php
+++ inc/class-ratelimiter.php
@@ -9,8 +9,10 @@ use WP;
 final class RateLimiter {
 	use Singleton;
 
-	public const RATELIMIT_PERIOD = 86400;
-	public const RATELIMIT_LIMIT  = 100;
+	public const RATELIMIT_PERIOD_VIEW   = 86400;
+	public const RATELIMIT_LIMIT_VIEW    = 100;
+	public const RATELIMIT_PERIOD_SEARCH = 3600;
+	public const RATELIMIT_LIMIT_SEARCH  = 10;
 
 	private ?Redis $redis = null;
 
@@ -35,21 +37,36 @@ final class RateLimiter {
 	}
 
 	public function parse_request( WP $wp ): void {
+		if ( ! $this->redis ) {
+			return;
+		}
+
 		/** @var scalar */
 		$post_type = $wp->query_vars['post_type'] ?? null;
+		/** @var mixed */
+		$criminal = $wp->query_vars['criminal'] ?? null;
+		/** @var mixed */
+		$cf = $wp->query_vars['cf'] ?? null;
 
-		if ( 'criminal' === $post_type && $this->redis ) {
+		$is_view   = 'criminal' === $post_type && is_string( $criminal ) && ! empty( $criminal );
+		$is_search = 'criminal' === $post_type && is_array( $cf ) && ! empty( $cf );
+
+		if ( $is_view || $is_search ) {
 			/** @var mixed */
 			$ip = $_SERVER['REMOTE_ADDR'] ?? null; // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPressVIPMinimum.Variables
 			if ( ! $ip || ! is_string( $ip ) ) {
 				return;
 			}
 
-			$ratelimit_period = (int) apply_filters( 'secenh_ratelimit_period', self::RATELIMIT_PERIOD );
-			$ratelimit_limit  = (int) apply_filters( 'secenh_ratelimit_limit', self::RATELIMIT_LIMIT );
+			$what   = $is_view ? 'view' : 'search';
+			$period = $is_view ? self::RATELIMIT_PERIOD_VIEW : self::RATELIMIT_PERIOD_SEARCH;
+			$limit  = $is_view ? self::RATELIMIT_LIMIT_VIEW : self::RATELIMIT_LIMIT_SEARCH;
 
-			$prefix = md5( (string) get_option( 'siteurl' ) );
-			$key    = $prefix . ':secenh_view_ratelimit_' . $ip;
+			$ratelimit_period = (int) apply_filters( "secenh_ratelimit_period_{$what}", $period );
+			$ratelimit_limit  = (int) apply_filters( "secenh_ratelimit_limit_{$what}", $limit );
+
+			$prefix = md5( (string) get_option( 'siteurl', '' ) );
+			$key    = sprintf( '%s:secenh_ratelimit:%s:%s', $prefix, $what, $ip );
 			if ( ! $this->redis->exists( $key ) ) {
 				$this->redis->set( $key, 1, $ratelimit_period );
 			} else {
